feff6f564a2d025a31969ae122cebd799238fe12
franknstin <anmhatre94@gmail.com>
2022-10-13 16:21:00 +0530

    added stg 02 data validation file

---
+++ src/cementStrength/pipeline/stage_02_data_validation.py
@@ -0,0 +1,21 @@
+from cementStrength.components.data_ingestion import DataIngestion 
+from cementStrength.config import ConfigurationManager
+from cementStrength import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+def main():
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.split_data_as_train_test()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
