e686cf5e0d3b9fef097fb9125f27fee3ebbda864
franknstin <anmhatre94@gmail.com>
2022-10-13 16:20:39 +0530

    added data validation entity

--- src/cementStrength/entity/config_entity.py
+++ src/cementStrength/entity/config_entity.py
@@ -9,3 +9,10 @@ DataIngestionConfig = namedtuple("DataIngestionConfig", [
     "ingested_data_train_dir",
     "ingested_data_test_dir"
     ])
+
+DataValidationConfig = namedtuple("DataValidationConfig", [
+    "data_validation_dir",
+    "schema_file_path",
+    "report_file_path",
+    "report_page_file_path"
+    ])
